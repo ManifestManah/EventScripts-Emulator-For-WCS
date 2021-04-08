1208d1f9ada45eb6b83b4d9526eac94f0dea3ab6
Manifest <christoffer.soendergaard@outlook.com>
2021-04-08 09:35:34 +0200

    Changed: Run.Py
    The Warcraft-Source Essentials seem to be using these specific argforms and that should be working, so I'm overwriting the change from previously today, we can always roll-back to the previous version should it not be working.

--- addons/source-python/plugins/es_emulator/eventscripts/esc/cmds/run.py
+++ addons/source-python/plugins/es_emulator/eventscripts/esc/cmds/run.py
@@ -8,12 +8,12 @@ import es
 @Command(syntax='<variable>', types=VAR, desc='Just runs a command-string inside of the variable.')
 def commandv(argv):
   stack.insertrawline(sv(argv[0]))
-
-@Command(argsfrom=3, syntax='<seconds> <commandstring>', types=FLOAT, desc='Will run <commandstring>, after <seconds> seconds.')
+  
+@Command(argsfrom=2, syntax='<seconds> <commandstring>', types=FLOAT, desc='Will run <commandstring>, after <seconds> seconds.')
 def delayed(argv, args):
   gamethread.delayed(argv[0], stack.queueline, (argv[1], argv[2:], None, esc.stack.currentblock, True))
 
-@Command(argsfrom=2, syntax='<commandstring>', desc='Adds a command to the end of the command queue.')
+@Command(argsfrom=1, syntax='<commandstring>', desc='Adds a command to the end of the command queue.')
 def soon(argv, args):
   queue(stack.queueline, (argv[0], argv[1:], None, esc.stack.currentblock, True))
 
